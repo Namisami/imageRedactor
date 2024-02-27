bd332fce72febcaa1cc986e906a125d42c3a16bc
Namisami <79804458+Namisami@users.noreply.github.com>
2024-02-27 19:10:57 +0300

    improve image show

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { Tabs } from 'antd';
+import { Tabs, Modal, Button } from 'antd';
 import type { TabsProps } from 'antd';
 import FileUpload from './pages/FileUpload/FileUpload';
 import URLUpload from './pages/URLUpload/URLUpload';
@@ -14,6 +14,7 @@ function App() {
     imageWidth: 0,
     imageHeight: 0,
   })
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   
   const uploadImageToCanvas = (file: File) => {
@@ -24,20 +25,20 @@ function App() {
       reader.onload = function(file) {
         const img = new Image();
         img.onload = function() {
-          let proportion = 1;
-          if (canvas.parentElement) {
-            proportion = canvas.parentElement.clientWidth / img.naturalWidth;
-          }
-          console.log(proportion)
-          if (proportion < 1) {
-            canvas.width = img.naturalWidth * proportion;
-            canvas.height = img.naturalHeight * proportion;
-          } else {
+          // let proportion = 1;
+          // if (canvas.parentElement) {
+          //   proportion = canvas.parentElement.clientWidth / img.naturalWidth;
+          // }
+          // if (proportion < 1) {
+          //   canvas.width = img.naturalWidth * proportion;
+          //   canvas.height = img.naturalHeight * proportion;
+          // } else {
             canvas.width = img.naturalWidth;
             canvas.height = img.naturalHeight;
-          }
+          // }
           ctx!!.drawImage(img, 0, 0, canvas.width, canvas.height);
-          setState({...state, imageWidth: img.naturalWidth, imageHeight: img.naturalHeight})
+          setIsModalOpen(false);
+          setState({...state, imageWidth: img.naturalWidth, imageHeight: img.naturalHeight});
         }
         if (file.target?.result) {
           img.src = file.target.result.toString();
@@ -61,15 +62,16 @@ function App() {
   ];
 
   const getPixelInfo = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    console.log(e)
     const canvas = canvasRef.current;
     if (canvas) {
       const ctx = canvas.getContext('2d');
-      const canvasX = canvas.offsetLeft;
-      const canvasY = canvas.offsetTop;
-      // const canvasX = canvas.getBoundingClientRect().left;
-      // const canvasY = canvas.getBoundingClientRect().top;
-      const mouseX = (e.pageX - canvasX);
-      const mouseY = (e.pageY - canvasY);
+      // const canvasX = canvas.offsetLeft;
+      // const canvasY = canvas.offsetTop;
+      // const mouseX = (e.pageX - canvasX);
+      // const mouseY = (e.pageY - canvasY);
+      const mouseX = e.nativeEvent.offsetX;
+      const mouseY = e.nativeEvent.offsetY;
       const p = ctx!!.getImageData(mouseX, mouseY, 1, 1).data;
       setState({...state, rgb: [p[0], p[1], p[2]], x: mouseX, y: mouseY}) 
     }
@@ -77,18 +79,34 @@ function App() {
 
   return (
     <div className="container">
-      <Tabs defaultActiveKey="1" items={ items } />
-      <div className="img-view">
-        <canvas onMouseMove={ getPixelInfo } className='canvas' ref={ canvasRef } />
+      <div className="app">
+        <div className="menu-panel">
+          <Button className="upload" type="primary" onClick={ () => setIsModalOpen(true) }>
+            Загрузить изображение
+          </Button>
+        </div>
+        <div className="work-panel">
+          <div className="img-view">
+            <canvas onMouseMove={ getPixelInfo } className='canvas' ref={ canvasRef } />
+          </div>
+          <div className="img-info">
+            <p>{ `RGB(${state.rgb})` }</p>
+            <p>{ `X${state.x}` }</p>
+            <p>{ `Y${state.y}` }</p>
+            <p>{ `Width: ${state.imageWidth}` }</p>
+            <p>{ `Height: ${state.imageHeight}` }</p>
+            <div style={{ background: `rgb(${[...state.rgb]})` }} className='color' />
+          </div>
+        </div>
       </div>
-      <div className="img-info">
-        <p>{ `RGB(${state.rgb})` }</p>
-        <p>{ `X${state.x}` }</p>
-        <p>{ `Y${state.y}` }</p>
-        <p>{ `Width: ${state.imageWidth}` }</p>
-        <p>{ `Height: ${state.imageHeight}` }</p>
-        <div style={{ background: `rgb(${[...state.rgb]})` }} className='color' />
-      </div>
+      <Modal 
+        title="Загрузка изображения" 
+        open={isModalOpen} 
+        onCancel={ () => setIsModalOpen(false) }
+        footer={[]}
+      >
+        <Tabs defaultActiveKey="1" items={ items } />
+      </Modal>
     </div>
   )
 }
